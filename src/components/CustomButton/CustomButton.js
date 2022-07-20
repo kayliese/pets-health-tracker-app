6f7c810988a3dca17850aa5be8fb1aae02e0cfa5
Kayliese Smith <49692061+kayliese@users.noreply.github.com>
2022-07-20 16:52:13 -0400

    Add files via upload

---
+++ src/components/CustomButton/CustomButton.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
+
+const CustomButton = ({onPress, text, type = 'PRIMARY', bgColor, fgColor}) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[
+        styles.container,
+        styles[`container_${type}`],
+        bgColor ? {backgroundColor: bgColor} : {},
+      ]}>
+      <Text
+        style={[
+          styles.text,
+          styles[`text_${type}`],
+          fgColor ? {color: fgColor} : {},
+        ]}>
+        {text}
+      </Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+
+    padding: 15,
+    marginVertical: 5,
+
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+
+  container_PRIMARY: {
+    backgroundColor: '#fe5e00',
+  },
+
+  container_SECONDARY: {
+    borderColor: '#fe5e00',
+    borderWidth: 2,
+  },
+
+  container_TERTIARY: {},
+
+  text: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+
+  text_SECONDARY: {
+    color: '#fe5e00',
+  },
+
+  text_TERTIARY: {
+    color: 'gray',
+  },
+});
+
+export default CustomButton;
